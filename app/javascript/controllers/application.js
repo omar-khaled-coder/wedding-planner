e336aaf186868a9aec34b653ab3886591f9d1bf4
omar abdrbuo <omarkh3010@gmail.com>
2023-03-29 17:30:30 +0800

    still working

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -11,3 +11,15 @@ export { application }
 $(document).on('turbo:load', function() {
   $('.carousel').carousel();
 });
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+const myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', event => {
+  // do something...
+})
