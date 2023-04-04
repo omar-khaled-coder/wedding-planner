1ffe5c5826cbae1e50b29f39c06ac1ef0b6a5f29
omar abdrbuo <omarkh3010@gmail.com>
2023-04-05 01:44:01 +0800

    validation 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
          validates :first_name, presence: { message: "First Name cannot be blank" }
+         validates :last_name, presence: { message: "Last Name cannot be blank" }
 end
